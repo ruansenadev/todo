a8c374c214e1aa1468930a9543a885337c147003
rsdev <ruansenadev@gmail.com>
2021-11-03 10:05:36 -0300

    react env

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import "./styles/common.scss";
+
+export function App() {
+  return <h1>oi react-dom</h1>;
+}
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const reactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+const isDevMode = process.env.NODE_ENV != "production";
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".jsx", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              isDevMode && require.resolve("react-refresh/babel")
+            ].filter(Boolean)
+          }
+        }
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  mode: isDevMode ? "development" : "production",
+  devtool: isDevMode ? "eval-source-map" : "source-map",
+  plugins: [
+    isDevMode && new reactRefreshWebpackPlugin(),
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html")
+    })
+  ].filter(Boolean),
+  devServer: {
+    static: path.resolve(__dirname, "public"),
+    hot: true
+  }
+}
