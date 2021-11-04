57607a0df00a4bde01b59d4eec11cfc9e2713d39
rsdev <ruansenadev@gmail.com>
2021-11-04 20:31:48 -0300

    task component view & task interface in component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { TaskForm } from "./components/TaskForm";
+import { Task } from "./components/Task";
 import "./styles/common.scss";
 
 export function App() {
   return (
-    <TaskForm />
+    <>
+      <TaskForm />
+      <Task />
+    </>
   );
 }
